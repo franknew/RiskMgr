b326795bb766213dd5fb34eea9bdae4a44838b9a
franknew <liuxiao.a.boy@gmail.com>
2015-07-07 17:40:56 +0800

    1

---
+++ src/background/RiskMgr/Api/CustomerApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiskMgr.Api
+{
+    public class CustomerApi
+    {
+        public int Add()
+        {
+            return -1;
+        }
+
+        public int Update()
+        {
+            return -1;
+        }
+
+        public bool Delete(int id)
+        {
+            return false;
+        }
+    }
+}
---
+++ src/background/RiskMgr/Api/LogonApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RiskMgr.Api
+{
+    public class LogonApi
+    {
+        public bool Logon(string username, string password)
+        {
+            return false;
+        }
+
+        public bool Logout(string username)
+        {
+            return false;
+        }
+    }
+}
