831233f4e901ff2c0e37913520d38d6c8de79c35
franknew <liuxiao.a.boy@gmail.com>
2015-07-24 18:35:04 +0800

    update

--- source/background/RiskMgr/Model/Entity/Asset.cs
+++ source/background/RiskMgr/Model/Entity/Asset.cs
@@ -8,31 +8,70 @@ namespace RiskMgr.Model
 {
     public class Asset : SimpleEntity
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public string Remark { get; set; }
         
-        public Int32? Type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Type { get; set; }
         
-        public Int32? Usage { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Usage { get; set; }
         
+        /// <summary>
+        /// 
+        /// </summary>
         public string Position { get; set; }
         
+        /// <summary>
+        /// 地址
+        /// </summary>
         public string Address { get; set; }
         
+        /// <summary>
+        /// 资产编号
+        /// </summary>
         public string Code { get; set; }
         
-        public Decimal? Area { get; set; }
+        /// <summary>
+        ///  面积
+        /// </summary>
+        public decimal? Area { get; set; }
         
-        public Decimal? RegPrice { get; set; }
+        /// <summary>
+        /// 注册价格
+        /// </summary>
+        public decimal? RegPrice { get; set; }
         
+        /// <summary>
+        /// 业主
+        /// </summary>
         public string OwnerID { get; set; }
         
+        /// <summary>
+        /// 购房人
+        /// </summary>
         public string BuyerID { get; set; }
         
-        public Int32? IssueType { get; set; }
+        /// <summary>
+        /// 办理事项
+        /// </summary>
+        public int? IssueType { get; set; }
         
-        public UInt64? IsTraded { get; set; }
+        /// <summary>
+        /// 是否交易
+        /// </summary>
+        public int? IsTraded { get; set; }
         
-        public UInt64? Eanbled { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Eanbled { get; set; }
         
     }
 }
--- source/background/RiskMgr/Model/Entity/Customer.cs
+++ source/background/RiskMgr/Model/Entity/Customer.cs
@@ -8,31 +8,70 @@ namespace RiskMgr.Model
 {
     public class Customer : SimpleEntity
     {
-        public UInt64? Gender { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int? Gender { get; set; }
         
-        public Int32? Marrage { get; set; }
+        /// <summary>
+        /// 婚姻状况
+        /// </summary>
+        public int? Marrage { get; set; }
         
-        public Int32? CardType { get; set; }
+        /// <summary>
+        /// 证件类型
+        /// </summary>
+        public int? CardType { get; set; }
         
+        /// <summary>
+        /// 证件号码
+        /// </summary>
         public string IdentityCode { get; set; }
         
+        /// <summary>
+        /// 电话
+        /// </summary>
         public string Phone { get; set; }
         
+        /// <summary>
+        /// 曾用名
+        /// </summary>
         public string OrignalName { get; set; }
         
+        /// <summary>
+        /// 曾用号码
+        /// </summary>
         public string OrignalIdentityCode { get; set; }
         
-        public Int32? BankType { get; set; }
+        /// <summary>
+        /// 银行类型
+        /// </summary>
+        public int? BankType { get; set; }
         
+        /// <summary>
+        /// 银行账号
+        /// </summary>
         public string BankCode { get; set; }
         
+        /// <summary>
+        /// 地址
+        /// </summary>
         public string Address { get; set; }
         
+        /// <summary>
+        /// 单位
+        /// </summary>
         public string WorkUnit { get; set; }
         
+        /// <summary>
+        /// 备注
+        /// </summary>
         public string Remark { get; set; }
         
-        public UInt64? Enabled { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Enabled { get; set; }
         
     }
 }
