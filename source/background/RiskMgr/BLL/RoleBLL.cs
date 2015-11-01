20f9c87c68d3d28dc1e08962f37456718309890f
franknew <liuxiao.a.boy@gmail.com>
2015-11-01 13:45:32 +0800

    update fuck

--- source/background/RiskMgr/BLL/RoleBLL.cs
+++ source/background/RiskMgr/BLL/RoleBLL.cs
@@ -1,4 +1,5 @@
-﻿using IBatisNet.DataMapper;
+﻿using DreamWorkflow.Engine;
+using IBatisNet.DataMapper;
 using RiskMgr.DAL;
 using RiskMgr.Form;
 using RiskMgr.Model;
@@ -14,17 +15,81 @@ namespace RiskMgr.BLL
     {
         public List<Role> Query(RoleQueryForm form)
         {
-            ISqlMapper mapper = null;
-            if (ServiceSession.Current.Context.Parameters.ContainsKey("Mapper"))
-            {
-                mapper = ServiceSession.Current.Context.Parameters["Mapper"] as ISqlMapper;
-            }
-            else
-            {
-                mapper = Mapper.Instance();
-            }
+            ISqlMapper mapper = Common.GetMapperFromSession();
             RoleDao dao = new RoleDao(mapper);
             return dao.Query(form);
         }
+
+        public List<string> GetUserSubUserIDs(string userid)
+        {
+            UserBLL userbll = new UserBLL();
+            ISqlMapper mapper = Common.GetMapperFromSession();
+            User_RoleDao urdao = new User_RoleDao(mapper);
+            RoleDao roledao = new RoleDao(mapper);
+            var urs = urdao.Query(new User_RoleQueryForm { UserID = userid });
+            List<string> useridlist = new List<string>();
+            //检查用户的角色有没有全数据权限
+            var roles = roledao.QueryRoleByUserID(userid);
+            foreach (var role in roles)
+            {
+                if (role.DataAccessType == (int)DataAccesssEnum.All)
+                {
+                    return null;
+                }
+            }
+            
+            useridlist.Add(userid);
+            List<Role> list = new List<Role>();
+            foreach (var ur in urs)
+            {
+                list.AddRange(GetAllSubRoles(ur.RoleID));
+            }
+            var roleids = (from r in list select r.ID).ToList();
+            if (roleids.Count == 0)
+            {
+                return useridlist;
+            }
+            var users = urdao.Query(new User_RoleQueryForm { RoleIDs = roleids });
+            useridlist.AddRange((from u in users select u.UserID).ToList());
+            return useridlist;
+        }
+
+        public List<Role> GetAllSubRoles(string roleid)
+        {
+            var allroles = TableCacheHelper.GetDataFromCache<Role>(typeof(RoleDao));
+            List<Role> list = new List<Role>();
+            var role = allroles.Find(t => t.ID == roleid);
+            GetSubRole_Resc(role, allroles, list);
+            return list;
+        }
+
+        public List<string> GetRoleUserIDs(string roleid)
+        {
+            ISqlMapper mapper = Common.GetMapperFromSession();
+            User_RoleDao urdao = new User_RoleDao(mapper);
+            var roles = GetAllSubRoles(roleid);
+            var roleids = (from r in roles select r.ID).ToList();
+            var urs = urdao.Query(new User_RoleQueryForm { RoleIDs = roleids });
+            return (from u in urs select u.UserID).ToList();
+        }
+
+        private void GetSubRole_Resc(Role role, List<Role> allroles, List<Role> list)
+        {
+            var subroles = allroles.FindAll(t => t.ParentID == role.ID);
+            foreach (var r in subroles)
+            {
+                if (!list.Contains(r))
+                {
+                    list.Add(r);
+                }
+                GetSubRole_Resc(r, allroles, list);
+            }
+        }
+    }
+
+    public enum DataAccesssEnum
+    {
+        Self = 1,
+        All = 2,
     }
 }
