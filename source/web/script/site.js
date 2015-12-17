7fb89367d43425365625ea40c3deb3a4a4f59067
viktorli <i@lizhenwen.com>
2015-12-17 19:43:56 +0800

    m

--- source/web/script/site.js
+++ source/web/script/site.js
@@ -3,11 +3,29 @@
 * @date    2015-07-08 21:54:42
 */
 
-(function  () {
-	var CDN = location.origin+location.pathname.replace(/\/.*\..*/,'/');;
+;(function  () {
+	var CDN = '//'+location.host+location.pathname.replace(/\/.*\..*/,'/');
+
+	var jq = '2.1.4',	//高版本jquery
+		jqLower = '1.11.3';	//低版本jquery，兼容<=ie8
+
+	var ua = {}, agent = navigator.userAgent;
+	if (window.ActiveXObject || window.msIsStaticHTML){
+		ua.ie = 6;
+		(window.XMLHttpRequest || (agent.indexOf('MSIE 7.0') > -1)) && (ua.ie = 7);
+		(window.XDomainRequest || (agent.indexOf('Trident/4.0') > -1)) && (ua.ie = 8);
+		(agent.indexOf('Trident/5.0') > -1) && (ua.ie = 9);
+		(agent.indexOf('Trident/6.0') > -1) && (ua.ie = 10);
+		(agent.indexOf('Trident/7.0') > -1) && (ua.ie = 11);
+
+		if(ua.ie == 6 || ua.ie == 7 || ua.ie == 8){
+			jq=jqLower;
+		}
+	}
+
 	seajs.config({
 		alias:{
-			'jquery':CDN+'script/lib/jquery-2.1.4/jquery.js',
+			'jquery':CDN+'script/lib/jquery-'+jq+'/jquery.js',
 			'bootstrap':CDN+'script/lib/bootstrap.js'
 		},
 		paths:{
@@ -16,6 +34,8 @@
 		}
 	});
 
-	seajs.use(['jquery','bootstrap','risk/unit/route','risk/components/user/index','risk/page/frame/index']);
+	seajs.use(['jquery','bootstrap','risk/unit/route','risk/components/user/index','risk/page/frame/index'],function() {
+
+	});
 
 })();
