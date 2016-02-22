1af73cc968f86914bfc84a19001a74b0f7e21d95
viktorli <i@lizhenwen.com>
2016-02-22 15:11:26 +0800

    fix bug

--- source/web/script/components/former/index.js
+++ source/web/script/components/former/index.js
@@ -315,6 +315,7 @@ define.pack("./index",["jquery","risk/unit/string","risk/components/msg/index","
 					itemState.tag = 'button';
 					break;
 				case 'date':
+					itemState.tag = 'input';
 					if (defaultValue && /^\d{13}$/.test(defaultValue)) {	//如果是纯13位的数字，标示为时间戳进行转化
 						defaultValue = RString.date(defaultValue,'yyyy-MM-dd');
 					}
--- source/web/script/components/former/src/index.js
+++ source/web/script/components/former/src/index.js
@@ -278,6 +278,7 @@ define(function(require, exports, module){
 					itemState.tag = 'button';
 					break;
 				case 'date':
+					itemState.tag = 'input';
 					if (defaultValue && /^\d{13}$/.test(defaultValue)) {	//如果是纯13位的数字，标示为时间戳进行转化
 						defaultValue = RString.date(defaultValue,'yyyy-MM-dd');
 					}
